5c5412837f77256fd74b75828b38b43e771ab57d
ayush4068@gmail.com <suppermink42>
2024-10-15 21:24:44 -0700

    example done

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
 class main{
 
     public static void main(String args[]){
-            
-
+        mainHelper c = new mainHelper("cools", "nets");
+        System.out.println(c);
+        
     }
 }
---
+++ mainHelper.java
@@ -0,0 +1,20 @@
+class mainHelper{
+    
+    String original;
+    String finalAc;
+
+    mainHelper(String original, String finalAc){
+        this.original = original;
+        this.finalAc = finalAc;
+    }
+
+  
+    
+
+    public String toString(){
+             return this.original + " " + this.finalAc;
+
+    } 
+
+
+}
