45e1b6abb024358d50cef0cc2d8a71ceeb50bcc0
suppermink42 <59209766+suppermink42@users.noreply.github.com>
2024-10-17 20:05:06 +0000

    so far

--- LetterInventory.java
+++ LetterInventory.java
@@ -1,1 +1,19 @@
-Le
+class LetterInventory{
+     
+     String data;
+     public int[] alphaKey = new int[26];
+
+     LetterInventory(String data){
+        this.data = data;
+     }
+
+    public Boolean isEmpty(){
+        if(data == null){
+            return true;
+        }
+        
+        return false;
+    }      
+
+
+}
--- main.java
+++ main.java
@@ -1,8 +1,36 @@
 class main{
 
+    public static void bytes(){
+        int[] array = new int[10];
+        int bytes1 = array.length * Integer.BYTES; 
+
+        System.out.println(bytes1);
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = i++;
+        }
+
+        bytes1 = array.length * Integer.BYTES;
+        System.out.println(bytes1);
+
+
+    }
+
+    public static void getChar(){
+
+        String exm = "hello";
+
+        for(int i = 0; i < exm.length(); i++){
+            System.out.println(exm.charAt(i));
+        }
+
+    }
+
     public static void main(String args[]){
-        mainHelper c = new mainHelper("cools", "nets");
-        System.out.println(c);
+        LetterInventory x;
+
+        bytes();
+
         
     }
 }
