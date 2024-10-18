c4d1c9b1844da73215654c35fa5cdc43f06e56db
suppermink42 <59209766+suppermink42@users.noreply.github.com>
2024-10-18 19:05:47 +0000

    done

--- LetterInventory.java
+++ LetterInventory.java
@@ -1,11 +1,25 @@
 class LetterInventory{
 
-    public String data;
-    public char[] alpha = new char[26];
+    private String data;
+     private int tempInt = 0;
+     private char tempChar;
+     private int[] alphaKey = new int[26];
 
-    LetterInventory(String data){
+     LetterInventory(String data){
         this.data = data;
+        
 
-    }
+            for(int i = 0; i < data.length(); i++){
+
+                tempChar = data.charAt(i);
+                tempInt = Character.getNumericValue(tempChar);
+            
+              
+                alphaKey[tempInt - 10]++;
+
+            }
+
+     }
+
 
 }
