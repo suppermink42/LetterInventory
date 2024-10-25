a6e294bc687adf8cd1a525813991b33185256402
ayush4068@gmail.com <suppermink42>
2024-10-24 19:20:23 -0700

    a

--- LetterInventory.java
+++ LetterInventory.java
@@ -8,23 +8,6 @@ class LetterInventory{
      private int[] alphaKey = new int[26];
 
      
-     /*
-     LetterInventory(String data){
-        this.data = data;
-        
-
-            for(int i = 0; i < data.length(); i++){
-
-                tempChar = data.charAt(i);
-                tempInt = Character.getNumericValue(tempChar);
-            
-                //Correct Syntax
-                alphaKey[tempInt - 10]++;
-
-            }
-
-     }
-    */
 
      LetterInventory(String data){
         this.data = data;
